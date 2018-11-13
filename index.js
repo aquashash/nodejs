76069d798565b71c6ad42816ac100939e6d3ad40
aquashash <tanujah@gmail.com>
2018-11-13 12:12:54 -0500

    Creating a simple web server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var http = require('http')
-var httpServer = http.createServer(function(req, res){
-    res.end('This is my first node app.');
+var url = require('url');
+
+var httpServer = http.createServer(function(req, res) {
+    var queryObject = url.parse(req.url, true).query;
+    var greeting = queryObject.greeting || 'Default greeting';
+    res.end('This is my first node app!! ' + greeting);
 });
 
 httpServer.listen(8080, function() {
